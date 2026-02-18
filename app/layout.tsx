c4401f8f5892d596eff4dabaaa8b62bf7940fd68
emilygtcorrea <emilygtcorrea@gmail.com>
2026-02-18 14:11:59 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Oracle Mab",
+  title: "Mab",
   description:
     "I've been expecting you."
 }
